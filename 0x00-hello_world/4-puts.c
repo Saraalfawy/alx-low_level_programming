762825f5ba7e3bcc7dfc7914c43786a258352713
Saraalfawy <saraalfawy833@gmail.com>
2023-06-16 13:03:20 -0700

    fder

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+  * main - entry point
+  *
+  * description: A C program that print with puts function
+  *
+  * return: always 0 (success)
+*/
 int main(void)
 {
 puts ("\"Programming is like building a multilingual puzzle\n");
