80371bad9978da0052c739aed9fdc156bf56a8d7
Saraalfawy <saraalfawy833@gmail.com>
2023-06-16 12:46:11 -0700

    ;lkjhg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-/**
-  * main - entry point
-  *
-  * description: A c program that prnts with put function
-  *
-  * retun: always 0 (success)
-*/
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle\n");
-return 0;
+
+int main(void)
+{
+puts ("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
+
 
 
 
