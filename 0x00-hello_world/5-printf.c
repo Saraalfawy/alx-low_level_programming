277e2364e0336bfe848c9ab00f5db6262aad03c1
Saraalfawy <saraalfawy833@gmail.com>
2023-06-16 13:12:23 -0700

    frew

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
+/**
+* main - entry point
+* description: A C program that print with printf function
+* return: always 0 (success)
+*/
+int main(void) 
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
 
