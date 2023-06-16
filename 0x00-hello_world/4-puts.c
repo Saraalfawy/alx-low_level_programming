a492afb35231f349d2840c354213d86e2449a3ea
Saraalfawy <saraalfawy833@gmail.com>
2023-06-16 11:59:26 -0700

    frew

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
 int main(void) {
-  printf("\"Programming is like building a multilingual puzzle\n");
-  return 0;
+printf("\"Programming is like building a multilingual puzzle\n");
+return 0;
 }
+
