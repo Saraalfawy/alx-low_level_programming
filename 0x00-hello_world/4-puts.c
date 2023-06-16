443b8f0541230641b641c1bfc6879d838bffca41
Saraalfawy <saraalfawy833@gmail.com>
2023-06-16 13:46:08 -0700

    gtre

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+
 /**
-  * main - entry point
-  *
-  * description: A C program that print with puts function
-  *
-  * return: always 0 (success)
-*/
+ *  main - entry point
+ *
+ *  description: A C program that prints with put function
+ *
+ *  Return: Always 0 (success)
+ */
+
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
