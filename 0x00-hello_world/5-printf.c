ff234b8f28522b1ed8efcb4d47371a20661e7d02
Saraalfawy <saraalfawy833@gmail.com>
2023-06-16 13:39:00 -0700

    dser

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
-* main - entry point
-* description: A C program that print with printf function
-* return: always 0 (success)
-*/
-int main(void) 
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
-
