4b4167d921d50ab831c87bc9b9b006899fe297c0
Saraalfawy <saraalfawy833@gmail.com>
2023-06-20 19:25:49 -0700

    bvfg

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * print_alphabet - 10 times the alphabet, in lowercase.
- */
+ * print_alphabet_x10 - function that print 10 times the alphabet, in lowercase.
+*/
 void print_alphabet_x10(void)
-{int line ch;
+
+{int line. ch;
 (for line = '0' ; line <= '10'; line++)
 {
 for (ch = 'a' ; ch <= 'z'; ch++)
