ea3c544de46c3c16100b43ac8d33d4b9702523c1
Saraalfawy <saraalfawy833@gmail.com>
2023-06-20 18:33:31 -0700

    dert

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  main - entry point
+ *  description: ' prints the alphabet, in lowercase'
+ *  Return: Always 0
+ */
+void print_alphabet(void);
+{
+  int ch;
+  for (ch = 'a' ; ch <= 'z'; ch++)
+    _putchar (ch);
+  _putchar ('\n')
+}
