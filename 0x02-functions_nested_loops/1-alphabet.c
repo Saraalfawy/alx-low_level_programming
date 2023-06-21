469478cddf9bf4c7f52493aa438cf69133683382
Saraalfawy <saraalfawy833@gmail.com>
2023-06-20 18:41:34 -0700

    fred

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *  description: ' prints the alphabet, in lowercase'
  *  Return: Always 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
   int ch;
   for (ch = 'a' ; ch <= 'z'; ch++)
